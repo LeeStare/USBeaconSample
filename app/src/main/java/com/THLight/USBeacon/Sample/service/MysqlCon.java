2568b9e26225d09a2a1cc94f5c4e1099fd9acd5a
Leestar <st973707@gmail.com>
2025-11-11 08:56:00 +0900

    [Delete] 刪除已修改完程式碼

--- app/src/main/java/com/THLight/USBeacon/Sample/service/MysqlCon.java
+++ app/src/main/java/com/THLight/USBeacon/Sample/service/MysqlCon.java
@@ -33,13 +33,6 @@ public class MysqlCon {
     String db_user = "sql12804805";
     String db_password = "8WkBTMX1nD";
 
-    /*String mysql_ip = "140.127.74.129";  //140.127.74.129
-    int mysql_port = 3306; // Port 預設為 3306  //3306
-    String db_name = "test";  //test
-    String url = "jdbc:mysql://"+mysql_ip+":"+mysql_port+"/"+db_name;
-    String db_user = "sirius";  //sirius
-    String db_password = "12345678"; //12345678*/
-
     public void run() {
         Log.v("DB","加載驅動成功");
 
@@ -51,147 +44,6 @@ public class MysqlCon {
             Log.e("DB","遠端連接失敗");
             Log.e("DB", e.toString());
         }*/
-    }
-
-    //--------------------------------------------------------------------------------------
-    //沐光的code
-    public boolean checkIfExistAccount(String account, String password, ApiHelper.BooleanCallback callback) {    //回傳帳密是否存在
-        boolean result = false;
-        try {
-            String url = "https://usbeaconfastapi.onrender.com/check_account";
-
-            OkHttpClient client = new OkHttpClient();
-            // 建立 JSON 物件
-            CheckAccountPasswordRequest input = new CheckAccountPasswordRequest(url, account, password);
-
-            // 建立請求
-            Request request = input.request;
-            // 設置回傳
-            Call call = client.newCall(request);
-
-            // 非同步執行
-            client.newCall(request).enqueue(new Callback() {
-                @Override
-                public void onFailure(@NonNull Call call, IOException e) {
-                    Log.e("API", "連線失敗: " + e.getMessage());
-                    callback.onResult(false);  // 回傳 false 表示失敗
-                }
-
-                @Override
-                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                    if (response.isSuccessful()) {
-                        String jsonResponse = response.body().string();
-                        try {
-                            JSONObject res = new JSONObject(jsonResponse);
-                            boolean exist = res.optBoolean("exist", false);
-                            callback.onResult(exist);
-                            Log.v("API", "帳號是否存在: " + exist);
-                        } catch (Exception e) {
-                            Log.e("API", "解析錯誤: " + e.getMessage());
-                            callback.onResult(false);
-                        }
-                    } else {
-                        Log.e("API", "伺服器回應失敗 code: " + response.code());
-                        callback.onResult(false);
-                    }
-                }
-            });
-
-            return result;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            Log.e("DB", "寫入資料失敗");
-            Log.e("DB", e.toString());
-        }
-        return result;
-    }
-
-    public boolean checkIfExistId(String id) {    //回傳此學號是否存在
-        boolean result = false;
-        try {
-            Connection con = DriverManager.getConnection(url, db_user, db_password);
-            String sql = "SELECT account FROM `userdata` WHERE account = '";
-            sql += id;
-            sql += "'";
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            if(rs.next()) {
-                result = true;
-                return result;
-            }
-            st.close();
-        } catch (SQLException e) {
-            //e.printStackTrace();
-            Log.e("DB", "寫入資料失敗");
-            Log.e("DB", e.toString());
-        }
-        return result;
-    }
-
-    public String getUserName(String id) {  //回傳使用者名稱
-        String result = "";
-        try {
-            Connection con = DriverManager.getConnection(url, db_user, db_password);
-            String sql = "SELECT user_name FROM `userdata` WHERE account = '" + id + "'";
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            if(rs.next()) {
-                result = rs.getString("user_name");;
-            }
-            st.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            Log.e("DB", "寫入資料失敗");
-            Log.e("DB", e.toString());
-        }
-        return result;
-    }
-
-    public void setFlag(String className , String user) {
-        if(getAccount(user).equals("410777000")) {
-            try {
-                Connection con = DriverManager.getConnection(url, db_user, db_password);
-                //設定所有課程flag=0
-                String sql_set_flag_0 = "UPDATE `class` SET flag = '0'";
-                Statement st_0 = con.createStatement();
-                st_0.executeUpdate(sql_set_flag_0);
-                //設定選擇課程flag=1
-                String sql_set_flag_1 = "UPDATE `class` SET flag = '1' WHERE classname = '" + className + "'";
-                Statement st_1 = con.createStatement();
-                st_1.executeUpdate(sql_set_flag_1);
-
-                String sql_set_flag_week_teacher = "UPDATE `" + getClassName() + "` SET " + getWeek_2() + " = '1' WHERE "
-                        + getWeek_2() + " IS NULL AND student_name = '郭教授'";
-                Statement st_week = con.createStatement();
-                st_week.executeUpdate(sql_set_flag_week_teacher);
-
-                st_0.close();
-                st_1.close();
-                st_week.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-                Log.e("DB", "寫入資料失敗");
-                Log.e("DB", e.toString());
-            }
-        }
-    }
-
-    public void setFlag0(String user) {
-        if(getAccount(user).equals("410777000")) {
-            try {
-                Connection con = DriverManager.getConnection(url, db_user, db_password);
-                //設定所有課程flag=0
-                String sql_set_flag_0 = "UPDATE `class` SET flag = '0'";
-                Statement st_0 = con.createStatement();
-                st_0.executeUpdate(sql_set_flag_0);
-                st_0.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-                Log.e("DB", "寫入資料失敗");
-                Log.e("DB", e.toString());
-            }
-        }
     }
 
     public String getWeek_1() {   //回傳老師本週flag=1且下週flag=NULL的週次
@@ -213,7 +65,6 @@ public class MysqlCon {
                 st.close();
             }
         } catch (SQLException e) {
-            e.printStackTrace();
             Log.e("DB", "寫入資料失敗");
             Log.e("DB", e.toString());
         }
@@ -240,32 +91,6 @@ public class MysqlCon {
         return getClass;
     }
 
-    public String getWeek_2() {
-        String week_num = "";
-        try {
-            Connection con = DriverManager.getConnection(url, db_user, db_password);
-            for (int i = 1; i <= 19; i++) {
-                String week1 = "week" + String.valueOf(i);
-                //String week2 = "week" + String.valueOf(i + 1);
-                String sql = "SELECT student_name," + week1 + " FROM `" + getClassName() + "` WHERE " + week1
-                        + " IS NULL AND student_name = '郭教授'";
-                Statement st = con.createStatement();
-                ResultSet rs = st.executeQuery(sql);
-                if (rs.next()) {
-                    //week_num = rs.getString(week1);
-                    week_num = week1;
-                    break;
-                }
-                st.close();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            Log.e("DB", "寫入資料失敗");
-            Log.e("DB", e.toString());
-        }
-        return week_num;
-    }
-
     public void roll_call(String userName) {
         try {
             Connection con = DriverManager.getConnection(url, db_user, db_password);
@@ -280,45 +105,6 @@ public class MysqlCon {
             Log.e("DB", e.toString());
         }
     }
-
-    public String getAccount(String user) {
-        String account = "";
-        try {
-            Connection con = DriverManager.getConnection(url, db_user, db_password);
-            String sql = "SELECT account,user_name FROM `userdata` WHERE user_name = '" + user + "'";
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            if(rs.next()) {
-                account = rs.getString("account");;
-            }
-            st.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            Log.e("DB", "寫入資料失敗");
-            Log.e("DB", e.toString());
-        }
-        return account;
-    }
-
-    public String getClassroom() {
-        String classroom = "";
-        try {
-            Connection con = DriverManager.getConnection(url, db_user, db_password);
-            String sql = "SELECT classroom FROM `class` WHERE flag = '1'";
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            if(rs.next()) {
-                classroom = rs.getString("classroom");;
-            }
-            st.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            Log.e("DB", "寫入資料失敗");
-            Log.e("DB", e.toString());
-        }
-        return classroom;
-    }
-
 
     //---------------------------------------------------------------------------------------------
     //以下為家誠的code
@@ -495,45 +281,6 @@ public class MysqlCon {
         return temp;
     }
 
-    //撰寫方法時，需要注意insert的欄位是否都有填滿
-    public void insertData(String account, String password, String user_name, String phone_number) {
-        try {
-            Connection con = DriverManager.getConnection(url, db_user, db_password);
-            String sql = "INSERT INTO `userdata` (`account`,`password`,`user_name`,`phone_number`) VALUES (" + account + ",'" + password + "','" + user_name + "'," + phone_number + ")";
-            Statement st = con.createStatement();
-            st.executeUpdate(sql);
-            st.close();
-            Log.v("DB", "寫入資料完成：" + account);
-            Log.v("DB", "寫入資料完成：" + password);
-            Log.v("DB", "寫入資料完成：" + user_name);
-            Log.v("DB", "寫入資料完成：" + phone_number);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            Log.e("DB", "寫入資料失敗");
-            Log.e("DB", e.toString());
-        }
-    }
-
-    public void insertClassData(String className, String classroom, String day, String time,int quantity, int flag) {
-        try {
-            Connection con = DriverManager.getConnection(url, db_user, db_password);
-            String sql = "INSERT INTO `class` (`className`,`classroom`,`day`,`time`,`quantity`,`flag`) VALUES (" + "'" +className + "','" + classroom + "','" + day + "','" + time + "'," + quantity +  "," + flag +")";
-            Statement st = con.createStatement();
-            st.executeUpdate(sql);
-            st.close();
-            Log.v("DB", "寫入資料完成：" + className);
-            Log.v("DB", "寫入資料完成：" + classroom);
-            Log.v("DB", "寫入資料完成：" + day);
-            Log.v("DB", "寫入資料完成：" + time);
-            Log.v("DB", "寫入資料完成：" + quantity);
-            Log.v("DB", "寫入資料完成：" + flag);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            Log.e("DB", "寫入資料失敗");
-            Log.e("DB", e.toString());
-        }
-    }
-
     public void createAttendanceTable(String className) {
         try {
             Connection con = DriverManager.getConnection(url, db_user, db_password);
@@ -638,26 +385,5 @@ public class MysqlCon {
         return temp;
     }
 
-    public ArrayList<String> getData_className() {
-        ArrayList<String> temp = new ArrayList<String>();
-
-        try {
-            Connection con = DriverManager.getConnection(url, db_user, db_password);
-            String sql = "SELECT className FROM class";
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-
-            while (rs.next())
-            {
-                String className = rs.getString("className");
-                temp.add(className);
-            }
-
-            st.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return temp;
-    }
 
 }
